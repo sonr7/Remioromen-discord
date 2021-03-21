268960a2b8acaec47c0e109d7e5bf75e364043f6
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-03-21 14:04:06 +0900

    Add files via upload

---
+++ Remio.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+import os
+
+token = os.environ.get('DISCORD_BOT_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('チャイム')
+
+@client.event
+async def on_message(messsge):
+    if startswith('#レミオロメン:') in message.content:
+        kaaaaaaaaaaaaaaaaaaaaaaasiiiiiiiiiiiiiiiiiiii = message.content.replace('#レミオロメン', '')
+        with open(kaaaaaaaaaaaaaaaaaaaaaaasiiiiiiiiiiiiiiiiiiii) as f:
+            kasi = f.read()
+        await message.channel.send(kasi)
+
+client.run(token)
